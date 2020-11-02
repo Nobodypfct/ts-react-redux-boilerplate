412edebe9c6538704d321a9e16fe04f18f59d0ce
Nickolaj Dojnikov <56079901+Nobodypfct@users.noreply.github.com>
2020-11-02 12:40:52 +0300

    feat: set up project, added dependencies, added some boilerplate

---
+++ src/store/reducer.ts
@@ -0,0 +1,47 @@
+import * as actionTypes from "./actionTypes"
+
+const initialState: ArticleState = {
+  articles: [
+    {
+      id: 1,
+      title: "post 1",
+      body:
+        "Repetition is the mother of learning",
+    },
+    {
+      id: 2,
+      title: "post 2",
+      body:
+        "Trying TS with redux",
+    },
+  ],
+}
+
+const reducer = (
+  state: ArticleState = initialState,
+  action: ArticleAction
+): ArticleState => {
+  switch (action.type) {
+    case actionTypes.ADD_ARTICLE:
+      const newArticle: IArticle = {
+        id: Math.random(), // not really unique
+        title: action.article.title,
+        body: action.article.body,
+      }
+      return {
+        ...state,
+        articles: state.articles.concat(newArticle),
+      }
+    case actionTypes.REMOVE_ARTICLE:
+      const updatedArticles: IArticle[] = state.articles.filter(
+        article => article.id !== action.article.id
+      )
+      return {
+        ...state,
+        articles: updatedArticles,
+      }
+  }
+  return state
+}
+
+export default reducer
